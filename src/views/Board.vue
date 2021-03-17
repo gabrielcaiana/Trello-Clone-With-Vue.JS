7caf93547682f8475d2ac9eaa34de750b9783500
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-03-17 12:55:48 -0300

    drag columns

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -5,98 +5,128 @@
         class="column"
         v-for="(column, $columnIndex) of board.columns"
         :key="$columnIndex"
-        @drop="moveTask($event, column.tasks)"
+        draggable
+        @drop="moveTaskOrColumn($event, column.tasks, $columnIndex)"
         @dragover.prevent
         @dragenter.prevent
+        @dragstart.self="pickupColumn($event, $columnIndex)"
       >
         <div class="flex items-center mb-2 font-bold">
           {{ column.name }}
         </div>
         <div class="list-reset">
-          <div 
+          <div
             class="task"
+            v-for="(task, $taskIndex) of column.tasks"
+            :key="$taskIndex"
             draggable
             @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
-            v-for="(task, $taskIndex) of column.tasks" 
-            :key="$taskIndex"
             @click="goToTask(task)"
-
+            @dragover.prevent
+            @dragenter.prevent
+            @drop.stop="moveTaskOrColumn($event, column.tasks, $columnIndex, $taskIndex)"
           >
             <span class="w-full flex-no-shrink font-bold">
-              {{task.name}}
+              {{ task.name }}
             </span>
-            <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
-              {{task.description}}
+            <p
+              v-if="task.description"
+              class="w-full flex-no-shrink mt-1 text-sm"
+            >
+              {{ task.description }}
             </p>
           </div>
+
           <input
-            type="text" 
+            type="text"
             class="block p-2 w-full bg-transparent"
             placeholder="+ Enter new task"
             @keyup.enter="createTask($event, column.tasks)"
-           >
+          />
         </div>
       </div>
+    </div>
 
-      <div class="task-bg" v-if="isTaskOpen" @click.self="close">
-        <router-view />
-      </div>
+    <div
+      class="task-bg"
+      v-if="isTaskOpen"
+      @click.self="close"
+    >
+      <router-view/>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-export default {
-  mounted() {
-    console.log(this.board)
-  },
 
+export default {
   computed: {
     ...mapState(['board']),
-
-    isTaskOpen() {
-      return this.$route.name == 'task'
+    isTaskOpen () {
+      return this.$route.name === 'task'
     }
   },
-
   methods: {
-    goToTask(task) {
-      this.$router.push({name : 'task', params: {id: task.id }})
+    goToTask (task) {
+      this.$router.push({ name: 'task', params: { id: task.id } })
     },
-
-    close() {
-      this.$router.push({name: 'board'})
+    close () {
+      this.$router.push({ name: 'board' })
     },
-
-    createTask(e, tasks) {
+    createTask (e, tasks) {
       this.$store.commit('CREATE_TASK', {
         tasks,
         name: e.target.value
       })
       e.target.value = ''
     },
-
-    pickupTask(e, taskIndex, fromColumnIndex) {
+    pickupTask (e, taskIndex, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'
       e.dataTransfer.dropEffect = 'move'
-      e.dataTransfer.setData('task-index', taskIndex)
-      e.dataTransfer.setData('from-column-index', fromColumnIndex)
-    },
 
-    moveTask(e, toTasks) {
+      e.dataTransfer.setData('from-task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'task')
+    },
+    pickupColumn (e, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'column')
+    },
+    moveTaskOrColumn (e, toTasks, toColumnIndex, toTaskIndex) {
+      const type = e.dataTransfer.getData('type')
+
+      if (type === 'task') {
+        this.moveTask(e, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTasks.length)
+      } else {
+        this.moveColumn(e, toColumnIndex)
+      }
+    },
+    moveTask (e, toTasks, toTaskIndex) {
       const fromColumnIndex = e.dataTransfer.getData('from-column-index')
       const fromTasks = this.board.columns[fromColumnIndex].tasks
-      const taskIndex = e.dataTransfer.getData('task-index')
+      const fromTaskIndex = e.dataTransfer.getData('from-task-index')
 
       this.$store.commit('MOVE_TASK', {
         fromTasks,
+        fromTaskIndex,
         toTasks,
-        taskIndex
+        toTaskIndex
+      })
+    },
+    moveColumn (e, toColumnIndex) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+
+      this.$store.commit('MOVE_COLUMN', {
+        fromColumnIndex,
+        toColumnIndex
       })
     }
   }
-};
+}
 </script>
 
 <style lang="css">
